1585ace8828ea1769e845e74dadc3ea1af2f869d
Robbie Greiner <robbieg@gmail.com>
2017-10-02 16:00:56 -0600

    iteration zero tests passing

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,16 @@
 export default class DistrictRepository {
+  constructor(dataPassedIn){
+    this.data = this.cleanUpData(dataPassedIn);
+    console.log(this.data);
+  }
 
+  cleanUpData(data){
+    return data.reduce( (accu, dataPiece) => {
+      if(!accu[dataPiece.Location]){
+        accu[dataPiece.Location] = [];
+      }
+      accu[dataPiece.Location].push(dataPiece);
+      return accu;
+    }, {})
+  }
 }
