a13d03b54b2f641b30f7358225f1f8be48334b3c
Sam Singer <samuel.singer1@gmail.com>
2017-10-03 14:28:48 -0600

    controls are firing and setting state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import Controls from './Controls';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      view: ''
+    }
+    this.updateView = this.updateView.bind(this)
+  }
+
+  updateView(buttonValue) {
+    console.log(buttonValue);
+    this.setState({view: buttonValue})
+  }
+
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+        <Controls updateView={this.updateView}/>
+      </div>
     );
   }
 }
