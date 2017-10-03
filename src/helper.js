a7881c8152857b46c7037ea6d264d5b4ad4ba9ef
Robbie Greiner <robbieg@gmail.com>
2017-10-03 12:54:56 -0600

    iteration one tests passing

--- src/helper.js
+++ src/helper.js
@@ -1,16 +1,25 @@
 export default class DistrictRepository {
   constructor(dataPassedIn){
     this.data = this.cleanUpData(dataPassedIn);
-    console.log(this.data);
   }
 
   cleanUpData(data){
     return data.reduce( (accu, dataPiece) => {
       if(!accu[dataPiece.Location]){
-        accu[dataPiece.Location] = [];
+        accu[dataPiece.Location] = {location: dataPiece.Location.toUpperCase(), data:{} }
       }
-      accu[dataPiece.Location].push(dataPiece);
+      accu[dataPiece.Location].data[dataPiece.TimeFrame] = Math.round(1000 * dataPiece.Data) / 1000 || 0;
       return accu;
     }, {})
+  }
+
+  findByName(location = undefined) {
+    if(location){
+      const dataKeys = Object.keys(this.data);
+      let found = dataKeys.find( item => {
+        return item.toUpperCase() === location.toUpperCase();
+      })
+      return this.data[found];
+    }
   }
 }
