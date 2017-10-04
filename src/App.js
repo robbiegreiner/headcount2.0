2bcd80d1da70fee36b7fe39232d064fb1991bd2b
Robbie Greiner <robbieg@gmail.com>
2017-10-03 19:50:27 -0600

    working on card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import Controls from './Controls';
 import CardContainer from './CardContainer';
+import kindergarten from '../data/kindergartners_in_full_day_program.js';
+import DistrictRepository from './helper.js';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      bigData: {},
+      cards: [],
       view: 'initial'
     }
     this.updateView = this.updateView.bind(this)
   }
 
   updateView(buttonValue) {
-    this.setState({view: buttonValue})
+    this.setState({ view: buttonValue})
+    this.setState({ bigData: new DistrictRepository(kindergarten) })
   }
 
   render() {
     return (
       <div>
         <Controls updateView={ this.updateView } />
-        <CardContainer view={ this.state.view } />
+        <CardContainer view={ this.state.view } bigData={ this.state.bigData } />
       </div>
 
     );
--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,36 +1,20 @@
 import React, { Component } from 'react';
-import DistrictRepository from './helper.js';
-import kindergarten from '../data/kindergartners_in_full_day_program.js';
-
 
 class CardContainer extends Component {
   constructor(){
     super();
-    this.allData = {};
-    this.state = {
-      cardData: [],
-    }
   }
 
-  getData() {
-    if(this.props.view === 'kindergarten'){
-      const ourData = new DistrictRepository(kindergarten);
-      this.allData = ourData;
-      console.log(this.allData);
-      this.playWithData();
-    }
-  }
-
-  playWithData() {
-    const myKeys = Object.keys(this.allData.data);
-    myKeys.map( key => {
-      return this.allData.data[key].location
-    })
+  formatData() {
+    const keys = Object.keys(this.state.bigData.data);
+    console.log(keys);
   }
 
   render() {
+    console.log(this.props.bigData);
     return (
       <div>
+        Card Container
       </div>
     );
   }
